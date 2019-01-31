dd92842b9f88f9f856ea40acd7ae87fbba96f4b3
plubojacky <plubojacky@olsonresearchca.com>
2019-01-31 15:48:17 -0800

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import Navbar from 'react-bootstrap/Navbar';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const setup = () => {
+    return shallow(<App />);
+}
+
+test('renders without crashing', () => {
+  
 });
+test ('contains `Navbar` element', () => {
+    const wrapper = setup();
+    const navbar = wrapper.find(Navbar);
+    expect (navbar.length).toBe(1);
+})
+test ('contains `Navbar.Brand` component', () => {
+    const wrapper = setup();
+    const nav_brand = wrapper.find(Navbar.Brand);
+    expect (nav_brand.length).toBe(1);
+})
+test ('Navbar.Brand has text `Classroom`', () => {
+    const wrapper = setup();
+    const nav_brand = wrapper.find(Navbar.Brand);
+    expect (nav_brand.text()).toContain("Classroom");
+})
